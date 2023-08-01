ac12405e08fbeab393185531c56c45e2b7a0cb58
Emmanuel <zabayo92@gmail.com>
2023-08-01 15:03:53 +0200

    Creating a function for generating board size from the user input

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
     createboard(16)
-    console.log("hi")
+    
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createboard(size);
+    })
 })
 
 function createboard(size){
@@ -11,7 +16,24 @@ function createboard(size){
     let numdivs = size * size;
     for (let i = 0; i < numdivs; i++){
         let div = document.createElement("div");
-        div.style.backgroundColor = "green";
+        div.addEventListener("mouseover", function(){
+            div.style.backgroundColor = "black"
+        })
         board.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function getSize(){
+    let input = prompt("Select the size of he board you want to use");
+    let message = document.querySelector("#message");
+    if (input == ""){
+        message.innerHTML = "Please the number of board size";
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = "The number should be between 1 and 100"
+    }
+    else{
+        message.innerHTML = "Great, You can now play!"
+        return input;
     }
 }
