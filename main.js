b4ec6ab40700bab3440173f05687f88a4be1ba6a
Emmanuel <zabayo92@gmail.com>
2023-08-01 13:04:01 +0200

    Creating a function for square size variation

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hi")
+document.addEventListener("DOMContentLoaded", function(){
+    createboard(16)
+    console.log("hi")
+})
+
+function createboard(size){
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
